a5f047d3c359fcc41329deab2a92e96eb9803ada
Sahill107 <sahill07@wharton.upenn.edu>
2021-12-17 04:54:16 -0500

    New cleaning

--- code/0-download.R
+++ code/0-download.R
@@ -1,12 +1,15 @@
 # load libraries
 library(tidyverse)
 
-# download case data from NY Times website
-url = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
-case_data_raw = read_csv(url)
+# download presidential election data (source: MIT Election Lab)
+election_raw = read_csv("/Users/sahill/OneDrive - PennO365/STAT 471/stat-471-final-project/data/raw/countypres_2000-2020.csv")
 
-# write raw data to file
-write_tsv(x = case_data_raw, file = "data/raw/case_data_raw.tsv")
+# download education, population, unemployment/income, and poverty data (source: USDA Economic Research Service)
+education_raw = read_csv("/Users/sahill/OneDrive - PennO365/STAT 471/stat-471-final-project/data/raw/Education.csv")
+population_raw = read_csv("/Users/sahill/OneDrive - PennO365/STAT 471/stat-471-final-project/data/raw/PopulationEstimates.csv")
+unemployment_raw = read_csv("/Users/sahill/OneDrive - PennO365/STAT 471/stat-471-final-project/data/raw/Unemployment.csv")
+poverty_raw = read_csv("/Users/sahill/OneDrive - PennO365/STAT 471/stat-471-final-project/data/raw/PovertyEstimates.csv")
 
-# download health rankings data from the web 
-# (omitted from this template)
+# download COVID and mask-use data (source: NYT)
+COVID_raw = read_csv("/Users/sahill/OneDrive - PennO365/STAT 471/stat-471-final-project/data/raw/us-covid-counties-2020.csv")
+masks_raw = read_csv("/Users/sahill/OneDrive - PennO365/STAT 471/stat-471-final-project/data/raw/mask-use-by-county.csv")
