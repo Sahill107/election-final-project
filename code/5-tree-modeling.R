8b6a40223037644eb2f946ebc6e9ab88030b6c6c
Mahima Sangli <msangli@wharton.upenn.edu>
2021-12-19 05:38:28 -0800

    [save optimal models]

--- code/5-tree-modeling.R
+++ code/5-tree-modeling.R
@@ -11,16 +11,182 @@ library(cowplot)       # for side-by-side plots
 ##Read in data
 train = read_csv('data/clean/train_data.csv')
 test = read_csv('data/clean/test_data.csv')
+
 ##Convert response to binary (Dem = 1, Rep = 0)
 train = train %>% 
   mutate(leading_party = as.numeric(leading_party == "Democrat"))
 test = test %>% 
   mutate(leading_party = as.numeric(leading_party == "Democrat"))
+
 ##Keep only relevant columns
 #container for column nails
 cols = colnames(train)
 train = train[cols[-c(1:14,41:44,55,62)]]
+test = test[cols[-c(1:14,41:44,55,62)]]
 
 ###Decision Tree
+##Growing the default tree
+#fit
+tree_fit = rpart(leading_party ~ .,
+                 method = 'class',
+                 parms = list(split = 'gini'),
+                 data = train)
+
+#plot
+rpart.plot(tree_fit)
+
+##Deepest possible tree
+#fit
+T_0 = rpart(leading_party ~ .,
+            method = 'class',
+            parms = list(split = 'gini'),
+            control = rpart.control(minsplit = 1,
+                                    minbucket = 1,
+                                    cp = 0),
+            data = train)
+#cp table
+cp_table = printcp(T_0) %>% 
+  as_tibble()
+
+##Pruning and cross-validation
+#cv plot
+cv_plot = cp_table %>%
+  filter(nsplit >= 2) %>%
+  ggplot(aes(x = nsplit+1, y = xerror,
+             ymin = xerror - xstd, ymax = xerror + xstd)) +
+  geom_point() + geom_line() +
+  geom_errorbar(width = 0.2) +
+  xlab("log10(Number of terminal nodes)") + ylab("CV error") +
+  geom_hline(aes(yintercept = min(xerror)), linetype = "dashed") +
+  scale_x_log10()
+theme_bw()
+#optimal tree
+set.seed(1)
+optimal_tree_info = cp_table %>%
+  filter(xerror - xstd < min(xerror)) %>%
+  arrange(nsplit) %>%
+  head(1)
+optimal_tree_info
+optimal_tree = prune(tree = T_0, cp = optimal_tree_info$CP)
+
 ###Random Forest
+##Default random forest
+#fit
+set.seed(1) # for reproducibility (DO NOT CHANGE)
+rf_fit = randomForest(factor(leading_party) ~ ., data = train)
+#OOB error
+def_OOB_plot = tibble(oob_error = rf_fit$err.rate[,"OOB"],trees = 1:500) %>%
+  ggplot(aes(x = trees, y = oob_error)) + geom_line() + theme_bw()
+
+##Tuning the random forest
+#identify best value of m
+set.seed(2) # for reproducibility (DO NOT CHANGE)
+
+#test out 5 different ms
+poss_m = seq(1,57,length.out=5)
+vec.OOB_err = c() #empty container to store OOB error values
+
+for(i in poss_m){
+  rf_fit2 = randomForest(factor(leading_party) ~ ., 
+                         mtry = i, 
+                         ntrees = 300,
+                         data = train)
+  OOB_err = rf_fit2$err.rate[,"OOB"][300]
+  vec.OOB_err = c(vec.OOB_err,OOB_err)
+}
+#create tibble
+m_OOB_err = tibble(m = poss_m,
+                   OOB_err = round(vec.OOB_err,4))
+#plot the data
+m_OOB_err_plot = m_OOB_err %>%
+  ggplot(aes(x = m, y = OOB_err)) +
+  geom_point() + geom_text(aes(label=m,hjust=2, vjust=1)) +
+  xlab("Value of m") + ylab("OOB Error") +
+  theme_bw()
+#tune using optimal m
+set.seed(2) # for reproducibility (DO NOT CHANGE)
+rf_fit3 = randomForest(factor(leading_party) ~ .,
+                       mtry = 15,
+                       importance = TRUE,
+                       data = train)
+#OOB error plot w/ optimal m
+optimal_OOB_err_plot = tibble(oob_error = rf_fit3$err.rate[,"OOB"],trees = 1:500) %>%
+  ggplot(aes(x = trees, y = oob_error)) + geom_line() + theme_bw() +
+  xlab("Number of Trees") + ylab("OOB Error")
+
 ###XGBoost
+##Fit boosted tree models with interaction depths 1, 2, and 3
+set.seed(1) # for reproducibility (DO NOT CHANGE)
+#interaction depth 1
+gbm_fit1 = gbm(leading_party ~ .,
+               distribution = "bernoulli",
+               n.trees = 1000,
+               interaction.depth = 1,
+               shrinkage = 0.1,
+               cv.folds = 5,
+               data = train)
+
+set.seed(1) # for reproducibility (DO NOT CHANGE)
+#interaction depth 2
+gbm_fit2 = gbm(leading_party ~ .,
+               distribution = "bernoulli",
+               n.trees = 1000,
+               interaction.depth = 2,
+               shrinkage = 0.1,
+               cv.folds = 5,
+               data = train)
+
+set.seed(1) # for reproducibility (DO NOT CHANGE)
+#interaction depth 3
+gbm_fit3 = gbm(leading_party ~ .,
+               distribution = "bernoulli",
+               n.trees = 1000,
+               interaction.depth = 3,
+               shrinkage = 0.1,
+               cv.folds = 5,
+               data = train)
+##Optimal fit
+#CV Errors
+ntrees = 1000
+
+CV_errors = tibble(Iteration = 1:ntrees, CV_1 = gbm_fit1$cv.error,
+                   CV_2 = gbm_fit2$cv.error, CV_3 = gbm_fit3$cv.error) 
+
+gbm_CV_errs = CV_errors %>%
+  ggplot() + 
+  geom_line(aes(x = Iteration, y = CV_1, color = 'depth 1')) +
+  geom_hline(yintercept=min(CV_errors$CV_1),linetype = 'dashed') +
+  
+  geom_line(aes(x = Iteration, y = CV_2, color = 'depth 2')) +
+  geom_hline(yintercept=min(CV_errors$CV_2),linetype = 'dashed') +
+  
+  geom_line(aes(x = Iteration, y = CV_3, color = 'depth 3')) + 
+  geom_hline(yintercept=min(CV_errors$CV_3),linetype = 'dashed') +
+  xlab("Number of Trees") + ylab("CV error") +
+  theme_bw()
+#Optimal number of trees
+gbm_fit_optimal = gbm_fit1
+optimal_num_trees = gbm.perf(gbm_fit1, plot.it = FALSE)
+optimal_num_trees
+
+###Interpretation
+##Random forest
+#Variabe importance
+var_imp = varImpPlot(rf_fit3,n.var=10)
+
+##Boosting
+#Relative Influence
+rel_inf = summary(gbm_fit_optimal, n.trees = optimal_num_trees, plotit = FALSE)[1:10,] %>%
+  as_tibble() 
+
+#Partial dependence plots
+#p1
+p1 = plot(gbm_fit_optimal, i.var = "pct_asian", n.trees = optimal_num_trees)
+
+#p2
+p2 = plot(gbm_fit_optimal, i.var = "log_traffic_volume", n.trees = optimal_num_trees)
+
+#p3
+p3 = plot(gbm_fit_optimal, i.var = "always", n.trees = optimal_num_trees)
+
+plot_grid(p1,p2,p3)
